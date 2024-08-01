a438e6b2e998a9f8d68cf4089c2eb6904c35321a
guialveira-professional <guilhermeaoliveira.profissional@gmail.com>
2024-08-01 16:41:28 -0300

    first commit

---
+++ api.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+from llama_index.core import VectorStoreIndex,  Settings
+from llama_index.embeddings.ollama import OllamaEmbedding
+from llama_index.llms.ollama import Ollama
+from llama_index.core import SimpleDirectoryReader
+from llama_index.readers.file import PDFReader
+
+app = Flask(__name__)
+embedding_model = OllamaEmbedding(model_name="llama3")
+
+@app.route('/embedding', methods=['POST'])
+def get_response():
+    parser = PDFReader()
+    file_extractor = {".pdf": parser}
+    documents = SimpleDirectoryReader(
+    "docs", file_extractor=file_extractor
+    ).load_data()
+    
+    ollama_embedding = OllamaEmbedding(
+        model_name="llama3",
+        base_url="http://localhost:11434", 
+        ollama_additional_kwargs={"mirostat": 0},
+    )
+    Settings.embed_model = ollama_embedding
+
+    Settings.llm = Ollama(model="llama3", temperature=0.2)
+
+    index = VectorStoreIndex.from_documents(
+        documents,
+    )
+
+    query_engine = index.as_query_engine()
+
+    return query_engine
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=11434)
